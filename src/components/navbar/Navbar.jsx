796cef4dd53b4bace7de8efe14d189705351f938
Kelviz <70589535+Kelviz@users.noreply.github.com>
2023-07-19 04:05:34 +0100

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   useEffect(() => {
     axios
-      .get("https://urch-django-4o3r3i18h-kelviz.vercel.app/api/category/")
+      .get("https://urch-django-api.vercel.app/api/category/")
       .then((response) => {
         setNavLinks(response.data);
         console.log(response.data);
